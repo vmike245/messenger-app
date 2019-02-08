c725e7936d214bd05f48f92122927f25f922b962
Michael Vellequette <michaelv245@yahoo.com>
2019-02-07 23:42:06 -0700

    Initial add message logic

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const portNumber = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+
+// Only needed in dev mode
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 let messages = [];
 
 app.get('/api/messages', (_, res) => {
--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    messages: []
+  }
+  postMessage = () => {
+    fetch('http://localhost:5000/api/messages',
+      { method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user: "1234", message: "words" }),
+      })
+      .then((res) => res.json())
+      .then(response => this.setState({messages: response}))
+      .catch((err) => console.log(err))
+  }
   render() {
+    console.log(this.state.messages)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p onClick={this.postMessage}>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
