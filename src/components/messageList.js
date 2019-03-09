c0508a6015973f9971e53c36c0d8caa18178d917
Michael Vellequette <michaelv245@yahoo.com>
2019-03-09 09:59:38 -0700

    Convert the messageList component to use hooks

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,55 +1,50 @@
-import React, { Component } from 'react';
+// Using Hooks
+
+import React, { useEffect } from 'react';
 import './messageList.css';
 
-export class MessageList extends Component {
-  lastMessage = React.createRef();
+export function MessageList(props) {
+  const { user, messages } = props;
+  const lastMessage = React.createRef()
 
-  // https://stackoverflow.com/questions/37620694/how-to-scroll-to-bottom-in-react
-  scrollToBottom = () => {
-    if (this.lastMessage) {
-      this.lastMessage.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+  useEffect(() => {
+    // https://stackoverflow.com/questions/37620694/how-to-scroll-to-bottom-in-react
+    const scrollToBottom = () => {
+      if (lastMessage) {
+        lastMessage.current.scrollIntoView({ behavior: 'smooth' });
+      }
+    };
+    scrollToBottom();
+  })
 
-  componentDidMount() {
-    this.scrollToBottom();
-  }
-
-  componentDidUpdate() {
-    this.scrollToBottom();
-  }
-
-  render() {
-    const { user, messages } = this.props;
-    return (
-      <div className='conversation'>
-        {messages.map((conversation, index, array) => {
-          const { message, user: sendingUser, date, id } = conversation;
-          const formattedDate = new Date(date).toDateString();
-          const messageClasses = 'message' + (sendingUser === user ? ' sent-message' : ' received-message');
-          // Saving reference of the last element in the message list so that we can scroll to it
-          if (index + 1 === array.length) {
-            return (
-              <div
-                className={messageClasses}
-                key={id}
-                ref={this.lastMessage}
-              >
-                {sendingUser !== user && <div className='user'>{sendingUser}</div>}
-                <div>{message}</div>
-                <div className='date'>{formattedDate}</div>
-              </div>
-            );
-          }
+  return (
+    <div className='conversation'>
+      {messages.map((conversation, index, array) => {
+        const { message, user: sendingUser, date, id } = conversation;
+        const formattedDate = new Date(date).toDateString();
+        const messageClasses = 'message' + (sendingUser === user ? ' sent-message' : ' received-message');
+        // Saving reference of the last element in the message list so that we can scroll to it
+        if (index + 1 === array.length) {
           return (
-            <div className={messageClasses} key={id}>
+            <div
+              className={messageClasses}
+              key={id}
+              ref={lastMessage}
+            >
               {sendingUser !== user && <div className='user'>{sendingUser}</div>}
               <div>{message}</div>
               <div className='date'>{formattedDate}</div>
             </div>
           );
-        })}
-      </div>
-    );
-  }
+        }
+        return (
+          <div className={messageClasses} key={id}>
+            {sendingUser !== user && <div className='user'>{sendingUser}</div>}
+            <div>{message}</div>
+            <div className='date'>{formattedDate}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
